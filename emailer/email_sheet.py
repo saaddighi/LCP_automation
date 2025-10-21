89fca3f0de0baa9f33537ae2c8e21e5c1aa1dc8b
saaddighi <118211198+saaddighi@users.noreply.github.com>
2025-10-21 15:36:14 +0100

    Update service account file path and spreadsheet ID

--- emailer/email_sheet.py
+++ emailer/email_sheet.py
@@ -1,11 +1,11 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-SERVICE_ACCOUNT_FILE = "path/account.json"
+SERVICE_ACCOUNT_FILE = "/home/saad/Documents/LCP_automation/emailer/account.json"
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly",
           "https://www.googleapis.com/auth/spreadsheets"]
 
 credentials = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 gc = gspread.authorize(credentials)
-spreadsheet_id = "1tZQzyJdZ85lWFd6dPBo7z17lEZ_6e7Xmeihk7Nr34lU"
+spreadsheet_id = "1CbyVgxTZ5Mz9gfbOzXbsZi37WZ7LuO8c2toQc0NkWdw"
 sheet = gc.open_by_key(spreadsheet_id).sheet1
